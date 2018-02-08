9bc7fa7550410b70ab29df7eceb3e885fcd71036
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-02-07 17:54:09 -0800

    Create POST endpoint for /recipes

--- server.js
+++ server.js
@@ -50,6 +50,23 @@ app.post('/shopping-list', jsonParser, (req, res) => {
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
+});
+
+app.post('/recipes', jsonParser, (req, res) => {
+    const requiredFields = ['name', 'ingredients'];
+
+    for(let i=0; i < requiredFields.length; i++){
+        const field = requiredFields[i];
+        if(!(field in req.body)){
+            const message = `Missing "${field}" in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+
+    const item = Recipes.create(req.body.name, req.body.ingredients);
+
+    res.status(201).send(item);
 })
 
 app.listen(process.env.PORT || 8080, () => {
